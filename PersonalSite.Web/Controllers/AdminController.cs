587f70bb5de8b34ed734ca7c9330af476ce9eae3
Glen Van Hoof <glenvanhoof@gmail.com>
2026-04-10 17:13:58 +0200

    Add Admin panel template

---
+++ PersonalSite.Web/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult ProjectTranslations()
+        {
+            return View();
+        }
+        public IActionResult Experiences()
+        {
+            return View();
+        }
+        public IActionResult Educations()
+        {
+            return View();
+        }
+        public IActionResult Certificates()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult ContactForms()
+        {
+            return View();
+        }
+    }
+}
