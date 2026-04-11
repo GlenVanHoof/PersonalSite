bfc5e0b8d0254066427212b4b89ffc118b947e5a
Glen Van Hoof <glenvanhoof@gmail.com>
2026-04-11 14:40:35 +0200

    Add about template

--- PersonalSite.Web/Controllers/AboutController.cs
+++ PersonalSite.Web/Controllers/AboutController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSite.Core.Interfaces;
+using PersonalSite.Web.Models;
 
 namespace PersoonlijkeSite.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly ISkillService _skillService;
+        private readonly IEducationService _educationService;
+        private readonly IExperienceService _experienceService;
+        private readonly ICertificateService _certificateService;
+
+        public AboutController(
+            ISkillService skillService,
+            IEducationService educationService,
+            IExperienceService experienceService,
+            ICertificateService certificateService)
         {
-            return View();
+            _skillService = skillService;
+            _educationService = educationService;
+            _experienceService = experienceService;
+            _certificateService = certificateService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new AboutViewModel
+            {
+                PictureURL = "images/about/profile-picture.png",
+                Skills = (await _skillService.GetAllSkillsAsync()).ToList(),
+                Educations = (await _educationService.GetAllEducationsAsync()).ToList(),
+                Experiences = (await _experienceService.GetAllExperiencesAsync()).ToList(),
+                Certificates = (await _certificateService.GetAllCertificatesAsync()).ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
