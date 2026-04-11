524bba8ac7a5f8abc9dd5a0d6364015a972b607a
Glen Van Hoof <glenvanhoof@gmail.com>
2026-04-11 16:50:20 +0200

    Add ProjectTranslation Create Update Delete in Admin panel + Update Admin nav-bar styling

--- PersonalSite.Web/Areas/Admin/Controllers/ProjectsController.cs
+++ PersonalSite.Web/Areas/Admin/Controllers/ProjectsController.cs
@@ -8,16 +8,31 @@ namespace PersonalSite.Web.Areas.Admin.Controllers
     public class ProjectsController : Controller
     {
         private readonly IProjectService _projectService;
+        private readonly IProjectTranslationService _projectTranslationService;
 
-        public ProjectsController(IProjectService projectService)
+        public ProjectsController(IProjectService projectService, IProjectTranslationService projectTranslationService)
         {
             _projectService = projectService;
+            _projectTranslationService = projectTranslationService;
         }
 
         public async Task<IActionResult> Index()
         {
             var projects = await _projectService.GetAllProjectsAsync();
             return View(projects);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var project = await _projectService.GetProjectByIdAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            var translations = await _projectTranslationService.GetTranslationsByProjectIdAsync(id);
+            ViewBag.Translations = translations;
+            return View(project);
         }
 
         public IActionResult Create()
@@ -83,6 +98,99 @@ namespace PersonalSite.Web.Areas.Admin.Controllers
             await _projectService.DeleteProjectAsync(id);
             TempData["SuccessMessage"] = "Project successfully deleted!";
             return RedirectToAction(nameof(Index));
+        }
+
+        // Translation Management Actions
+        public async Task<IActionResult> CreateTranslation(int projectId)
+        {
+            var project = await _projectService.GetProjectByIdAsync(projectId);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            var translation = new ProjectTranslation { ProjectId = projectId };
+            ViewBag.Project = project;
+            return View(translation);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateTranslation(ProjectTranslation translation)
+        {
+            if (ModelState.IsValid)
+            {
+                await _projectTranslationService.CreateTranslationAsync(translation);
+                TempData["SuccessMessage"] = "Translation successfully created!";
+                return RedirectToAction(nameof(Details), new { id = translation.ProjectId });
+            }
+
+            var project = await _projectService.GetProjectByIdAsync(translation.ProjectId);
+            ViewBag.Project = project;
+            return View(translation);
+        }
+
+        public async Task<IActionResult> EditTranslation(int id)
+        {
+            var translation = await _projectTranslationService.GetTranslationByIdAsync(id);
+            if (translation == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _projectService.GetProjectByIdAsync(translation.ProjectId);
+            ViewBag.Project = project;
+            return View(translation);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditTranslation(int id, ProjectTranslation translation)
+        {
+            if (id != translation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _projectTranslationService.UpdateTranslationAsync(translation);
+                TempData["SuccessMessage"] = "Translation successfully updated!";
+                return RedirectToAction(nameof(Details), new { id = translation.ProjectId });
+            }
+
+            var project = await _projectService.GetProjectByIdAsync(translation.ProjectId);
+            ViewBag.Project = project;
+            return View(translation);
+        }
+
+        public async Task<IActionResult> DeleteTranslation(int id)
+        {
+            var translation = await _projectTranslationService.GetTranslationByIdAsync(id);
+            if (translation == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _projectService.GetProjectByIdAsync(translation.ProjectId);
+            ViewBag.Project = project;
+            return View(translation);
+        }
+
+        [HttpPost, ActionName("DeleteTranslation")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteTranslationConfirmed(int id)
+        {
+            var translation = await _projectTranslationService.GetTranslationByIdAsync(id);
+            if (translation == null)
+            {
+                return NotFound();
+            }
+
+            var projectId = translation.ProjectId;
+            await _projectTranslationService.DeleteTranslationAsync(id);
+            TempData["SuccessMessage"] = "Translation successfully deleted!";
+            return RedirectToAction(nameof(Details), new { id = projectId });
         }
     }
 }
