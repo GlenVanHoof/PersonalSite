f7ef6bd0b818fd72a80d6953c0a37c7fd9e7d78e
Glen Van Hoof <glenvanhoof@gmail.com>
2026-04-15 14:57:41 +0200

    Add website styling

--- PersonalSite.Web/Models/AboutViewModel.cs
+++ PersonalSite.Web/Models/AboutViewModel.cs
@@ -9,5 +9,6 @@ namespace PersonalSite.Web.Models
         public List<Education> Educations { get; set; } = new List<Education>();
         public List<Experience> Experiences { get; set; } = new List<Experience>();
         public List<Certificate> Certificates { get; set; } = new List<Certificate>();
+        public string? Description { get; set; }    
     }
 }
