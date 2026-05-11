0f36d03e3571d9d339ab43f420d31b989ddc4b1f
Glen Van Hoof <glenvanhoof@gmail.com>
2026-05-11 09:51:52 +0200

    Update about-skill-section +Update carousel-styling

--- PersonalSite.Core/Enums/SkillType.cs
+++ PersonalSite.Core/Enums/SkillType.cs
@@ -2,8 +2,8 @@ namespace PersonalSite.Core.Enums;
 
 public enum SkillType
 {
-    Technical,
-    Soft,
-    Tool,
-    Language
+    Technical = 1,
+    Tool = 2,
+    Soft = 3,
+    Language = 4
 }
